f2ee712a13831467a5e36af44efba0097a4f629a
Krzysztof Karczewski <kr.karczewski@gmail.com>
2026-02-01 01:35:28 +0100

    chore: remove unused import

--- composeApp/src/commonMain/kotlin/io/github/karczews/brainagator/ui/screens/GameSelectionScreen.kt
+++ composeApp/src/commonMain/kotlin/io/github/karczews/brainagator/ui/screens/GameSelectionScreen.kt
@@ -20,8 +20,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-
-import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.withStyle
