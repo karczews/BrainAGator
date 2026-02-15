3eeff54ce030bd93d0823bb94670ac4e6925df43
Krzysztof Karczewski <kr.karczewski@gmail.com>
2026-02-15 11:14:45 +0100

    chore: group dependencies in androidApp

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -46,20 +46,22 @@ kotlin {
 
 
 dependencies {
+    implementation(projects.composeApp)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)/*
-    implementation(libs.androidx.compose.ui.tooling.preview)*/
+    implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.material3)
-    implementation(projects.composeApp)
+
+
     testImplementation(libs.junit)
+
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
     androidTestImplementation(libs.androidx.testExt.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)/*
-    debugImplementation(libs.androidx.compose.ui.tooling)*/
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
 }
