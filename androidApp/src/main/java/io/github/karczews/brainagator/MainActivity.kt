d55c209d7c2fb58b2eb2b4c6a9394c3043da8795
Krzysztof Karczewski <kr.karczewski@gmail.com>
2026-02-15 11:08:20 +0100

    style: remove unused imports

--- androidApp/src/main/java/io/github/karczews/brainagator/MainActivity.kt
+++ androidApp/src/main/java/io/github/karczews/brainagator/MainActivity.kt
@@ -4,11 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
-import io.github.karczews.brainagator.App
-import io.github.karczews.brainagator.theme.BrainagatorTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
