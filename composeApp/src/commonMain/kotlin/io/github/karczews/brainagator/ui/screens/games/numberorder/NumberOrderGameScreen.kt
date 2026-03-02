68910733105493dfdd806cdfaa63259504b9ad3e
Krzysztof Karczewski <kr.karczewski@gmail.com>
2026-03-03 00:41:35 +0100

    fix: remove invalid import

--- composeApp/src/commonMain/kotlin/io/github/karczews/brainagator/ui/screens/games/numberorder/NumberOrderGameScreen.kt
+++ composeApp/src/commonMain/kotlin/io/github/karczews/brainagator/ui/screens/games/numberorder/NumberOrderGameScreen.kt
@@ -31,7 +31,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.animateItem
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
