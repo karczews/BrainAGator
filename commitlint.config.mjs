0bc2c4c348644c7543885c67f9c4a62a0ec206cf
Krzysztof Karczewski <kr.karczewski@gmail.com>
2026-03-07 16:26:59 +0100

    chore: add package.json with commitlint and husky dependencies

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,1 +1,8 @@
-export default { extends: ['@commitlint/config-conventional'] };
+const allowedTypes = ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'chore', 'ci'];
+
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', allowedTypes],
+  },
+};
